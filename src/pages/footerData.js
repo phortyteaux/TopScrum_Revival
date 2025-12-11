c68bf5521ef0ee005d95ec1d9cbd4c9ee24fa6a6
Max <64117041+phortyteaux@users.noreply.github.com>
2025-12-10 16:34:55 -0800

    moved information
    moved contact link to About Us

--- src/pages/footerData.js
+++ src/pages/footerData.js
@@ -3,13 +3,13 @@ export const footerSections = [
     title: "About Us",
     links: [
       { label: "GitHub", path: "https://github.com/phortyteaux/TopScrum_Revival" },
+      { label: "Contact", path: "/contact" },
     ],
   },
   {
     title: "Support",
     links: [
       { label: "How to use", path: "/demo" },
-      { label: "Contact", path: "/contact" },
     ],
   },
 ];
