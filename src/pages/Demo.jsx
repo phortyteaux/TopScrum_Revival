804e403e2a2d0974cf776f2d005fe4d62c8f81f6
Max <64117041+phortyteaux@users.noreply.github.com>
2025-12-10 16:04:16 -0800

    added some information to Demo
    added text based instructions for using our webapp. Will add images later after we have applied some css

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,5 +1,32 @@
 export default function Demo() {
   return (
-    <p> this is a test </p>
+  <div>
+    <h1>Step One:</h1>
+    <p>First, create an account from our sign up page. Afterwards, you will be automatically logged in.</p>
+    <br></br>
+    <h1>Step Two:</h1>
+    <p>Next, create a deck by navigating to the Create Deck link at the top of the page. Once you have provided a deck title and a description, click the button below the input fields to make your deck. 
+    Now, you may either make additional decks or if you're ready to start adding cards to your deck(s), navigate to the link titled My Decks at the top of the page.
+    </p>
+    <br></br>
+    <h1>Step Three:</h1>
+    <p>On this page, you can see the deck or decks you have created and can select decks that you wish to delete. 
+    To add cards to a deck, simply left click on the deck you wish to add a card to, and then select the Add New Card button.
+    You will be redirected to the form to enter information regarding the card you are adding to your deck. First, enter the text for the front of the card in the appropriate field
+    and then repeat for the information you wish to store on the back of the card. Should you wish to upload an image for your card, you can navigate to the image by clicking Browse...
+    Lastly, you may select the checkbox to automatically add this card to your favorites within your deck before pressing Add Card.
+    </p>
+    <br></br>
+    <h1>Step Four:</h1>
+    <p>To review your deck, click the Review Deck button. This will switch to review mode where you can test yourself on the cards in your deck. By default, the review mode will present you with
+    the front of each card, and provide you with buttons to select if you answered the card correctly or incorrectly. Additionally, you may select Multiple Choice mode at the top of the page via 
+    a checkbox. In this mode, the webapp will present you with multiple choice selections taken from the backs of other cards in your deck.
+    </p>
+    <br></br>
+    <h1>Step Five:</h1>
+    <p>Lastly, you can view your study stats by clicking View Stats. Here you can view information from previous review sessions such as the total number of cards in each deck, how many times you
+    have reviewed each deck, and what amount of answers you got correct versus incorrect.
+    </p>
+  </div>
   );
 }
